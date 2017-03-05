03130bc19a85dd3383b44088efb3f4f53a9d1197
Agastya Darma <gedeagas22@gmail.com>
2017-03-05 17:27:26 +0700

    Updates

---
+++ src/pemesanantiket/ViewPesanTiket.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemesanantiket;
+
+/**
+ *
+ * @author gedeagas
+ */
+public class ViewPesanTiket {
+      private Tiket tiket;
+    private Customer customer;
+    public void viewTiket(){
+   
+    }
+    
+}
